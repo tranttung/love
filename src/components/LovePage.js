7739a87c0f572ffe9add4beae6156329bc34a69d
tungtt1 <tungtt1@runsystem.net>
2024-06-24 11:57:38 +0700

    first commit

---
+++ src/components/LovePage.js
@@ -0,0 +1,15 @@
+// src/components/LovePage.js
+import React from 'react';
+import './LovePage.css';
+
+const LovePage = () => {
+  return (
+    <div className="love-page">
+      <div className="message">
+        Yêu công chúa xin đẹp lung linh tuyệt vời của anhhh nhất
+      </div>
+    </div>
+  );
+};
+
+export default LovePage;
